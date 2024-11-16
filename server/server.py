8d8a8b48b7a09d628040124fefd8b4e8c1019f5e
Leo Cheng <leocheng333375749@gmail.com>
2024-11-15 21:07:19 -0500

    model to api integration

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import time
+import hand_gesture
 
 app = Flask(__name__)
 
@@ -7,9 +8,21 @@ app = Flask(__name__)
 @app.route('/test_post', methods=['POST'])
 def post():
     data = request.get_json()
-    print('data :: ', data)
+    img = hand_gesture.base64_to_image(data['image'])
+    gesture = hand_gesture.hand_inputs(img)
+
+    print('gesture :: ', gesture)
     response = {
         'message': 'post test',
+        'data': gesture
+    }
+    return jsonify(response)
+
+
+@app.route('/test_get', methods=['GET'])
+def get():
+    response = {
+        'message': 'get test',
         'data': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
     }
     return jsonify(response)
